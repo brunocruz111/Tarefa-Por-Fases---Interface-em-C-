a6638cd1301c2a336b3e383a9fdbd747847016e2
brunocruz111 <brunocruz@alunos.utfpr.edu.br>
2025-11-24 09:51:35 -0300

    Update Agendamento.cs

--- src/Fase6/Domain/Agendamento.cs
+++ src/Fase6/Domain/Agendamento.cs
@@ -3,7 +3,11 @@ using System.Globalization;
 
 namespace Fase06.Domain;
 
-public readonly record struct Agendamento(string Nome, string Servico, DateTime Quando)
+public readonly record struct Agendamento(
+    int Id,
+    string Nome,
+    string Servico,
+    DateTime Quando)
 {
     public string FormatadoPtBr() =>
         Quando.ToString("dd/MM 'às' HH:mm", new CultureInfo("pt-BR"));
